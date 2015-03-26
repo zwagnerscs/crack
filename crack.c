9c1cb406e77b6e9565d86bd6150a49c480a82830
Barry Brown <profbbrown@gmail.com>
2015-03-26 12:00:30 -0700

    Opens files for reading

--- crack.c
+++ crack.c
@@ -3,5 +3,27 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Hello!\n");
+    if (argc != 3)
+    {
+        printf("You must supply a hashfile and a dictionary file\n");
+        return 1;
+    }
+    
+    FILE *hf, *df;
+    hf = fopen(argv[1], "r");
+    if (!hf)
+    {
+        printf("Couldn't open %s for reading\n", argv[1]);
+        return 2;
+    }
+    
+    df = fopen(argv[2], "r");
+    if (!df)
+    {
+        printf("Couldn't open %s for reading\n", argv[2]);
+        return 3;
+    }
+    
+    fclose(hf);
+    fclose(df);
 }
